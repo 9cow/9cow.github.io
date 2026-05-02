6dfbfc58ba07e76478a2365cb016a6ebd7ddce31
db <altsm.net@gmail.com>
2026-05-02 01:27:09 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-1
+import * as boo from './js/boo.js';
+
+const boton = document.getElementById('btn-generar');
+const display = document.getElementById('resultado');
+
+const manejarClick = () => {
+  const bytes = boo.getRandomBytes(16);
+  const hex = boo.bytesToHex(bytes);
+
+  display.textContent = `Generado: ${hex}`;
+};
+
+boton.addEventListener('click', manejarClick);
